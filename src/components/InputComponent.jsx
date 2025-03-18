69f725e20134bad0129b23eee5a5424a3545bd3a
Nemes Tamás <nemestamas94@gmail.com>
2025-03-18 18:19:41 +0100

    create input component and count characters

---
+++ src/components/InputComponent.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const InputComponent = () => {
+  const [text, setText] = useState("almaasdsad")
+  
+  return (
+    <div>
+        <input 
+            type="text" 
+            value={text}
+            onChange={(e)=>setText(e.target.value)}
+        />
+        <p>The text is {text.length} character long.</p>
+    </div>
+  )
+}
+export default InputComponent
